849d259ef6e41fff67947f88390c0266fc73e046
Takashi Ishii <takashi5510@gmail.com>
2021-03-16 17:44:16 +0900

    first commit

---
+++ src/test/kotlin/jp/co/asnew/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.co.asnew.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
